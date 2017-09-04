0e705a3d1bac7ee570a5f46d32f1326a3ee4e5c2
Dante <dante.pawlow@gmail.com>
2017-09-03 22:23:16 -0300

    reading from stdin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,60 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Tests t = new Tests();
-        t.main();
+        Integer[] lista = null;
+        Scanner in = new Scanner(System.in);
+
+        int i = 0;
+        int a = 0;
+        int cero = 0;
+        while (in.hasNextInt()) {
+
+            i = in.nextInt();
+            a = in.nextInt();
+
+            if (a == 0 && i == 0){
+                break;
+            }
+
+            ArrayList<Agente> agentes = new ArrayList<>();
+            for (int k = 0; k < i; k++) {
+                Agente agente = new Agente(k);
+                agentes.add(agente);
+            }
+
+            for (int k = 0; k < a; k++) {
+                int idAgente = in.nextInt();
+                int encuesta = in.nextInt();
+
+                System.out.println("id agente: " + idAgente + ", encuesta: " + encuesta);
+
+                if (encuesta < 0) {
+                    agentes.get(idAgente-1).agregarDesconfiable(-encuesta);
+                } else {
+                    agentes.get(idAgente-1).agregarConfiable(encuesta);
+                }
+            }
+
+            Backtracking bt = new Backtracking(i, agentes);
+            BacktrackingPodado1 btp1 = new BacktrackingPodado1(i, agentes);
+            BacktrackingPodado2 btp2 = new BacktrackingPodado2(i, agentes);
+
+            Integer maxConfiablesBT = bt.calcularMaxConfiables();
+            Integer maxConfiablesBTP1 = btp1.calcularMaxConfiables();
+            Integer maxConfiablesBTP2 = btp2.calcularMaxConfiables();
+
+            if ( maxConfiablesBT.equals(maxConfiablesBTP1) && maxConfiablesBT.equals(maxConfiablesBTP2)){
+                System.out.println(maxConfiablesBT);
+            } else {
+                System.out.println("Error! Las respuestas no coinciden: " + maxConfiablesBT + " " + maxConfiablesBTP1 + " " + maxConfiablesBTP2);
+            }
+        }
+        in.close();
 
     }
 }
--- src/com/company/Tests.java
+++ src/com/company/Tests.java
@@ -7,6 +7,7 @@ public class Tests {
     public void main() {
         Random r = new Random();
 
+
         System.out.println("Agentes crecientes, encuestas aleatorias");
         for (int repeticiones = 0; repeticiones <= 100; repeticiones++) {
             for (int i = 1; i <= 400; i++) {
@@ -54,16 +55,104 @@ public class Tests {
                 fin = System.nanoTime();
 
                 System.out.println("BactrackingPodado2 encuestas aleatorias;" + i + ";" + btMaxP2 + ";" + (fin - inicio) + ";");
-
             }
-
         }
 
+        System.out.println("Agentes crecientes, encuestas positivas");
+        for (int repeticiones = 0; repeticiones <= 100; repeticiones++) {
+            for (int i = 1; i <= 100; i++) {
+                long inicio;
+                long promedio;
+                long fin;
+
+                ArrayList<Agente> agentes = new ArrayList<>();
+                for (int k = 1; k <= i; k++) {
+                    Set<Integer> confiables = new HashSet<>();
+                    Set<Integer> noConfiables = new HashSet<>();
+
+                    Integer maxConfiables = r.nextInt(i) + 1;
+
+                    for (int j = 1; j <= maxConfiables; j++) {
+                        confiables.add(r.nextInt(i) + 1);
+                    }
+
+                    Agente agente = new Agente(k, confiables, noConfiables);
+                    agentes.add(agente);
+                }
+
+                Backtracking bt = new Backtracking(i, agentes);
+                BacktrackingPodado1 btp1 = new BacktrackingPodado1(i, agentes);
+                BacktrackingPodado2 btp2 = new BacktrackingPodado2(i, agentes);
+
+                inicio = System.nanoTime();
+                Integer btMax = bt.calcularMaxConfiables();
+                fin = System.nanoTime();
+
+                System.out.println("Bactracking encuestas positivas;" + i + ";" + btMax + ";" + (fin - inicio) + ";");
+
+                inicio = System.nanoTime();
+                Integer btMaxP1 = btp1.calcularMaxConfiables();
+                fin = System.nanoTime();
+
+                System.out.println("BactrackingPodado1 encuestas positivas;" + i + ";" + btMaxP1 + ";" + (fin - inicio) + ";");
+
+                inicio = System.nanoTime();
+                Integer btMaxP2 = btp2.calcularMaxConfiables();
+                fin = System.nanoTime();
+
+                System.out.println("BactrackingPodado2 encuestas positivas;" + i + ";" + btMaxP2 + ";" + (fin - inicio) + ";");
+            }
+        }
+
+        System.out.println("Agentes crecientes, encuestas restringidas");
+        for (int repeticiones = 0; repeticiones <= 40; repeticiones++) {
+            for (int i = 1; i <= 40; i++) {
+                long inicio;
+                long promedio;
+                long fin;
+
+                ArrayList<Agente> agentes = new ArrayList<>();
+                for (int k = 1; k <= i; k++) {
+                    Set<Integer> confiables = new HashSet<>();
+                    Set<Integer> noConfiables = new HashSet<>();
+
+                    Integer maxConfiables = r.nextInt((int)Math.ceil(i/50)+1) + 1;
+                    Integer maxNoConfiables = r.nextInt((int)Math.ceil(i/50)+1) + 1;
+
+                    for (int j = 1; j <= maxConfiables; j++) {
+                        confiables.add(r.nextInt(i) + 1);
+                    }
+                    for (int j = 1; j <= maxNoConfiables; j++) {
+                        noConfiables.add(r.nextInt(i) + 1);
+                    }
+                    noConfiables.remove(confiables);
+                    Agente agente = new Agente(k, confiables, noConfiables);
+                    agentes.add(agente);
+                }
+
+                Backtracking bt = new Backtracking(i, agentes);
+                BacktrackingPodado1 btp1 = new BacktrackingPodado1(i, agentes);
+                BacktrackingPodado2 btp2 = new BacktrackingPodado2(i, agentes);
+
+                inicio = System.nanoTime();
+                Integer btMax = bt.calcularMaxConfiables();
+                fin = System.nanoTime();
+
+                System.out.println("Bactracking encuestas restringidas;" + i + ";" + btMax + ";" + (fin - inicio) + ";");
+
+                inicio = System.nanoTime();
+                Integer btMaxP1 = btp1.calcularMaxConfiables();
+                fin = System.nanoTime();
+
+                System.out.println("BactrackingPodado1 encuestas restringidas;" + i + ";" + btMaxP1 + ";" + (fin - inicio) + ";");
+
+                inicio = System.nanoTime();
+                Integer btMaxP2 = btp2.calcularMaxConfiables();
+                fin = System.nanoTime();
+
+                System.out.println("BactrackingPodado2 encuestas restringidas;" + i + ";" + btMaxP2 + ";" + (fin - inicio) + ";");
+            }
+        }
 
     }
-
-
-
-
-
 }
